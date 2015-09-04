902c35c5fc8b081c7c7f9a94d2a183ee4200067f
kate-crosby <kate.crosby@gmail.com>
2015-09-04 13:39:19 -0700

    plots and things

--- SweeD.R
+++ SweeD.R
@@ -1,23 +1,70 @@
-
-ten_thousand <- read.table("out_10000_to_R.txt", header=TRUE)
-
 library(qqman)
-library(extrafont)
-loadfonts()
-names(ten_thousand)=c('CHR', 'BP', "P", "Alpha")
 
-df <- read.table("cluster2_average10", header = TRUE, sep="\t")
+ten_thousand.1 <- read.table("out_10000_cluster1_to_R.txt", header=TRUE)
+names(ten_thousand.1)=c('CHR', 'BP', "P", "Alpha")
 
-names(df)=c('CHR', 'BP', "bfs", "P")
+df.1 <- read.table("cluster1_averaged", header = TRUE, sep="\t")
 
-par(mfrow=c(1,2))
-manhattan(ten_thousand, ylim = c(0, 400), suggestiveline = F, genomewideline = F, 
-          col = c("yellow3", "yellow"), logp=FALSE,
-          ylab="Composite Likelihood of Yellow SS group", 
-          cex=1.5, family="Helvetica Neue Thin", main ="Scans for positive selection post80s")
+names(df.1)=c('CHR', 'BP', "bfs", "P")
 
-manhattan(df, ylim = c(0, 50), suggestiveline = F, genomewideline = F, col = c("yellow3", "yellow"), logp=FALSE,
-          ylab=expression("log"[10]*" Bayes Fators yellow SS group"), cex=1.5, 
-          family="Helvetica Neue Thin", main="Average of 10 Bayenv runs")
+
+ten_thousand.2 <- read.table("out_10000_to_R.txt", header=TRUE)
+names(ten_thousand.2)=c('CHR', 'BP', "P", "Alpha")
+
+df.2 <- read.table("cluster2_average10", header = TRUE, sep="\t")
+
+names(df.2)=c('CHR', 'BP', "bfs", "P")
+
+
+ten_thousand.3 <- read.table("out_10000_cluster3_R.txt", header=TRUE)
+names(ten_thousand.3)=c('CHR', 'BP', "P", "Alpha")
+
+df.3 <- read.table("cluster3_averaged", header = TRUE, sep="\t")
+
+names(df.3)=c('CHR', 'BP', "bfs", "P")
+
+
+ten_thousand.5 <- read.table("out_10000_cluster5_R.txt", header=TRUE)
+names(ten_thousand.5)=c('CHR', 'BP', "P", "Alpha")
+
+df.5 <- read.table("cluster5_averaged", header = TRUE, sep="\t")
+
+names(df.5)=c('CHR', 'BP', "bfs", "P")
+
+
+
+par(mfrow=c(2,4))
+manhattan(ten_thousand.2, ylim = c(0, 400), suggestiveline = F, genomewideline = F, 
+          col = c("yellow4", "yellow2"), logp=FALSE,
+          ylab="Composite Likelihood of Yellow SS Group", 
+          cex=1.5, main ="")
+manhattan(df.2, ylim = c(0, 50), suggestiveline = F, genomewideline = F, col = c("yellow4", "yellow2"), logp=FALSE,
+          ylab=expression("log"[10]*" Bayes Fators yellow SS Group"), cex=1.5, 
+          main="")
+
+manhattan(ten_thousand.1, ylim = c(0, 400), suggestiveline = F, genomewideline = F, 
+          col = c("red4", "red2"), logp=FALSE,
+          ylab="Composite Likelihood of Tropical Group", 
+          cex=1.5, main ="")
+manhattan(df.1, ylim = c(0, 50), suggestiveline = F, genomewideline = F, col = c("red4", "red2"), logp=FALSE,
+          ylab=expression("log"[10]*" Bayes Fators Tropical Group"), cex=1.5, 
+          main="")
+
+manhattan(ten_thousand.3, ylim = c(0, 400), suggestiveline = F, genomewideline = F, 
+          col = c("green4", "green2"), logp=FALSE,
+          ylab="Composite Likelihood of Green Early SS group", 
+          cex=1.5, main ="")
+manhattan(df.3, ylim = c(0, 50), suggestiveline = F, genomewideline = F, col = c("green4", "green2"), logp=FALSE,
+          ylab=expression("log"[10]*" Bayes Fators Green Early SS Group"), cex=1.5, 
+          main="")
+
+manhattan(ten_thousand.5, ylim = c(0, 400), suggestiveline = F, genomewideline = F, 
+          col = c("blue4", "blue2"), logp=FALSE,
+          ylab="Composite Likelihood of NSS group", 
+          cex=1.5, main ="")
+manhattan(df.5, ylim = c(0, 50), suggestiveline = F, genomewideline = F, col = c("blue4", "blue2"), logp=FALSE,
+          ylab=expression("log"[10]*" Bayes Fators NSS Group"), cex=1.5, 
+          main="")
+
 
 
